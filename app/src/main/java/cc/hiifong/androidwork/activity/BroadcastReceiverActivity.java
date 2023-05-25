1bbae27887e1b7b4d0a3225cfa9c763b499e3bc1
hiifong <i@hiif.ong>
2023-05-25 17:35:08 +0800

    Finished

--- app/src/main/java/cc/hiifong/androidwork/activity/BroadcastReceiverActivity.java
+++ app/src/main/java/cc/hiifong/androidwork/activity/BroadcastReceiverActivity.java
@@ -2,15 +2,101 @@ package cc.hiifong.androidwork.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import cc.hiifong.androidwork.R;
 
 public class BroadcastReceiverActivity extends AppCompatActivity {
+    private String result;
+    private TextView textView;
+    private EditText et_num,et_msg;
+    private Button bt_send;
+
+    static final String BROADCAST_ACTION = "android.provider.Telephony.SMS_RECEIVER";
+    SmsReceiver smsReceiver;
+    IntentFilter intentFilter;
+
+    class SmsReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            SmsMessage msg = null;
+            textView.setText("");
+            result = "";
+            if (null != bundle){
+                Object[] smsObj = (Object[]) bundle.get("pdus");
+                for (Object object : smsObj) {
+                    msg =SmsMessage.createFromPdu((byte[]) object);
+                    Date date = new Date(msg.getTimestampMillis()); // 时间
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String receiverTime = format.format(date);
+                    result += "\n number:" + msg.getOriginatingAddress()
+                            + " \n body:" + msg.getDisplayMessageBody()
+                            + " \n time:" + receiverTime;
+                }
+                Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+                textView.setText(result);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_receiver);
+
+        textView = findViewById(R.id.msg);
+        et_num = findViewById(R.id.et_num);
+        et_msg = findViewById(R.id.et_msg);
+        bt_send = findViewById(R.id.btn_send);
+
+        intentFilter = new IntentFilter(BROADCAST_ACTION);
+        smsReceiver = new SmsReceiver();
+        registerReceiver(smsReceiver, intentFilter);
+
+
+        bt_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone_num = et_num.getText().toString().trim();
+                String sms_content = et_msg.getText().toString().trim();
+                if (phone_num.equals("")){
+                    Toast.makeText(BroadcastReceiverActivity.this, "请输入来信手机号!",Toast.LENGTH_SHORT).show();
+                }else {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    if (sms_content.length() > 70){
+                        List<String> content = smsManager.divideMessage(sms_content);
+                        for (String sms: content){
+                            smsManager.sendTextMessage(phone_num, null,
+                                    sms, null, null);
+                        }
+                    }else {
+                        smsManager.sendTextMessage(phone_num, null,sms_content,null,null);
+                    }
+                    Toast.makeText(BroadcastReceiverActivity.this, "短信已发送!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(smsReceiver);
+        super.onDestroy();
     }
 }
--- app/src/main/java/cc/hiifong/androidwork/activity/NotificationActivity.java
+++ app/src/main/java/cc/hiifong/androidwork/activity/NotificationActivity.java
@@ -1,16 +1,82 @@
 package cc.hiifong.androidwork.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import cc.hiifong.androidwork.R;
 
 public class NotificationActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+    private Button btn;
+
+    NotificationManager manager;
+    NotificationChannelGroup channelGroup;
+    NotificationChannel notificationChannel;
+    Notification notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        textView = findViewById(R.id.please_input_ms);
+        editText = findViewById(R.id.ms);
+        btn = findViewById(R.id.send);
+
+        //管理通知类
+        manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        //通知渠道组用于管理通知渠道
+        channelGroup = new NotificationChannelGroup("GroupId1", "GroupName1");
+        //创建通知渠道组
+        manager.createNotificationChannelGroup(channelGroup);
+        //创建通知渠道
+        notificationChannel = new NotificationChannel("ChannelId1", "ChannelName1", NotificationManager.IMPORTANCE_HIGH);
+        notificationChannel.enableLights(true);//允许使用LED消息提示灯
+        notificationChannel.enableVibration(true);//允许振动
+        notificationChannel.setLightColor(Color.YELLOW);//设置LED提示灯为黄色
+        notificationChannel.setVibrationPattern(new long[]{ 0, 100, 100, 100 });//设置振动的频率
+        notificationChannel.setGroup("GroupId1");//设置通知渠道组
+        manager.createNotificationChannel(notificationChannel);
+
+        // 通知跳转动作
+        Intent intent = new Intent(this, BroadcastReceiverActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        //构造通知
+        notification = new NotificationCompat.Builder(this, "ChannelId1")
+                .setContentTitle("上课了上课了！")
+                .setContentText("你老师喊你回直播间上网课啦！")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        int second = Integer.parseInt(editText.getText().toString());
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                manager.notify(1, notification);//发通知
+            }
+        });
+
     }
 }
