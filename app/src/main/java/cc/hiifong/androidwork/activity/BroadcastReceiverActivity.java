3a42b7a9e1013ac19aa07ca081333f5721f20229
hiifong <i@hiif.ong>
2023-05-25 10:31:26 +0800

    Update

---
+++ app/src/main/java/cc/hiifong/androidwork/activity/BroadcastReceiverActivity.java
@@ -0,0 +1,16 @@
+package cc.hiifong.androidwork.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cc.hiifong.androidwork.R;
+
+public class BroadcastReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast_receiver);
+    }
+}
