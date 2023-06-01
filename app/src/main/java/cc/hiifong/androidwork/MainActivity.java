1b93cdde4d42f5f6d279bf1842b88bdd1cc702ea
hiifong <i@hiif.ong>
2023-06-01 10:36:49 +0800

    Update

--- app/src/main/java/cc/hiifong/androidwork/MainActivity.java
+++ app/src/main/java/cc/hiifong/androidwork/MainActivity.java
@@ -1,14 +1,201 @@
 package cc.hiifong.androidwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import cc.hiifong.androidwork.service.MusicService;
 
 public class MainActivity extends AppCompatActivity {
+    // MusicBox接收器所能响应的Action
+    public static final String MUSICBOX_ACTION = "cc.hiifong.androidwork.MUSICBOX_ACTION";
+    // MusicService接收器所能响应的Action
+    public static final String MUSICSERVICE_ACTION = "cc.hiifong.androidwork.MUSICSERVICE_ACTION";
+    // 初始化flag
+    public static final int STATE_NON = 0x100;
+    // 播放的flag
+    public static final int STATE_PLAY = 0x101;
+    // 暂停的flag
+    public static final int STATE_PAUSE = 0x102;
+    // 停止放的flag
+    public static final int STATE_STOP = 0x103;
+    // 播放上一首的flag
+    public static final int STATE_PREVIOUS = 0x104;
+    // 播放下一首的flag
+    public static final int STATE_NEXT = 0x105;
+    // 播放状态
+    public int state;
+
+    Button btn_pre,btn_next,btn_state;
+    public static SeekBar skbMusic;
+    // 获取界面中显示的歌曲标题,作者文本框
+    TextView song_name, singer_name;
+    String[] song_list = new String[] {"手心的蔷薇", "这,就是爱", "一路生花"};
+    String[] singer_list = new String[] {"林俊杰&邓紫棋", "张杰", "周深"};
+    // 是否正在播放
+    boolean isPlaying = false;
+    MusicBoxReceiver receiver;
+    IntentFilter filter;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        skbMusic  = findViewById(R.id.sk);
+        skbMusic.setOnSeekBarChangeListener(changeListener);
+        btn_next = findViewById(R.id.btn_next);
+        btn_state = findViewById(R.id.btn);
+        btn_pre = findViewById(R.id.btn_pre);
+        btn_next.setOnClickListener(listener);
+        btn_state.setOnClickListener(listener);
+        btn_pre.setOnClickListener(listener);
+        song_name = findViewById(R.id.song_name);
+        singer_name = findViewById(R.id.singer_name);
+        // 注册接收器
+        receiver = new MusicBoxReceiver();
+        filter = new IntentFilter();
+        filter.addAction(MUSICBOX_ACTION);
+        registerReceiver(receiver, filter);
+        intent = new Intent(this, MusicService.class); // 指定要开启的服务
+        // 情况一: 判断后台服务是否已开启,如开启则恢复当前播放进度和状态
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo serviceInfo: manager.getRunningServices(Integer.MAX_VALUE)){
+            if ("cc.hiifong.administrator.androidwork.MusicService".equals(serviceInfo.service.getClassName()) &&
+            serviceInfo.started
+            ){
+                skbMusic.setMax(MusicService.mediaPlayer.getDuration());
+                skbMusic.setProgress(MusicService.mediaPlayer.getCurrentPosition());
+                song_name.setText(song_list[MusicService.current]); // 更新音乐标题
+                singer_name.setText(singer_list[MusicService.current]); // 更新音乐作者
+                state = STATE_PLAY;
+            }
+            if (MusicService.mediaPlayer.isPlaying()){
+                btn_state.setText("暂停");
+                isPlaying = true;
+            }
+        }
+        // 情况二: 未开启过服务则开启服务
+        song_name.setText(song_list[0]);
+        singer_name.setText(singer_list[0]);
+        state = STATE_NON;
+        startService(intent);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn_next: // 下一首
+                    btn_state.setText("暂停");
+                    state = STATE_NEXT;
+                    sendBroadcastToService(state);
+                    isPlaying = true;
+                    break;
+                case R.id.btn: // 播放或暂停
+                    if(!isPlaying){
+                        btn_state.setText("暂停");
+                        state = STATE_PLAY;
+                        sendBroadcastToService(state);
+                        isPlaying = true;
+                    }else {
+                        btn_state.setText("播放");
+                        state = STATE_PAUSE;
+                        sendBroadcastToService(state);
+                        isPlaying = false;
+                    }
+                    break;
+                case R.id.btn_pre: // 上一首
+                    btn_state.setText("暂停");
+                    state = STATE_PREVIOUS;
+                    sendBroadcastToService(state);
+                    isPlaying = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+    SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            // 当拖动停止后.控制mediaPlayer播放指定位置的音乐
+            if (state != STATE_NON){
+                MusicService.mediaPlayer.seekTo(seekBar.getProgress());
+            }else {
+                seekBar.setProgress(0);
+            }
+            MusicService.isChanging = false;
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            MusicService.isChanging = true;
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    class MusicBoxReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // 获取Intent中current消息,current代表当前正在播放的歌曲
+            int current = intent.getIntExtra("current", -1);
+            song_name.setText(song_list[current]);
+            singer_name.setText(singer_list[current]);
+        }
+    }
+
+    protected void sendBroadcastToService(int state){
+        Intent intent1 = new Intent();
+        intent1.setAction(MUSICSERVICE_ACTION);
+        intent1.putExtra("control", state);
+        // 向后台Service发送播放控制的广播
+        sendBroadcast(intent1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 关联和添加菜单项
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add(0, 1, 1, "停止");
+        menu.add(0, 2, 2, "退出");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == 1) {
+            btn_state.setText("播放");
+            sendBroadcastToService(STATE_STOP);
+            isPlaying = false;
+        } else if (id == 2) {
+            stopService(intent);
+            this.finish();;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/cc/hiifong/androidwork/service/MusicService.java
@@ -0,0 +1,183 @@
+package cc.hiifong.androidwork.service;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cc.hiifong.androidwork.MainActivity;
+
+public class MusicService extends Service {
+    // MusicBox接收器所能响应的Action
+    public static final String MUSICBOX_ACTION = "cc.hiifong.androidwork.MUSICBOX_ACTION";
+    // MusicService接收器所能响应的Action
+    public static final String MUSICSERVICE_ACTION = "cc.hiifong.androidwork.MUSICSERVICE_ACTION";
+    // 初始化flag
+    public static final int STATE_NON = 0x100;
+    // 播放的flag
+    public static final int STATE_PLAY = 0x101;
+    // 暂停的flag
+    public static final int STATE_PAUSE = 0x102;
+    // 停止放的flag
+    public static final int STATE_STOP = 0x103;
+    // 播放上一首的flag
+    public static final int STATE_PREVIOUS = 0x104;
+    // 播放下一首的flag
+    public static final int STATE_NEXT = 0x105;
+
+    TimerTask timerTask;
+
+    public static boolean isChanging = false; // 互斥变量,防止定时器与SeekBar拖动时进度冲突
+    // 创建一个媒体播放器的对象
+    public static MediaPlayer mediaPlayer;
+    // 创建一个Asset管理器的对象
+    AssetManager assetManager;
+    // 存放音乐名的数组
+    String[] musics = new String[] {"shouxindeqiangwei.mp3", "thisislove.mp3", "yilushenghua.mp3"};
+    // 当前播放的音乐
+    public static int current;
+    // 当前播放状态
+    int state = STATE_NON;
+    // 记录Timer运行状态
+    boolean isTimerRunning = false;
+    MusicServiceReceiver receiver;
+    IntentFilter filter;
+
+    public void onCreate() {
+        super.onCreate();
+        // 注册接收器
+        receiver = new MusicServiceReceiver();
+        filter = new IntentFilter();
+        filter.addAction(MUSICSERVICE_ACTION);
+        registerReceiver(receiver, filter);
+        mediaPlayer = new MediaPlayer();
+        assetManager = getAssets();
+        current = 0;
+        // 为mediaPlayer的完成事件创建监听器
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                current++;
+                prepareAndPlay(current);
+            }
+        });
+    }
+
+    /**
+     * 装载和播放音乐
+     * @param index int index 播放第几音乐的索引
+     * @return
+     */
+    protected void prepareAndPlay(int index) {
+        if (isTimerRunning) { // 如果Timer正在运行
+            timerTask.cancel(); // 取消定时器
+            isTimerRunning = false;
+        }
+        if (index > musics.length - 1) {
+            current = index = 0;
+        }
+        if (index < 0) {
+            current = index = musics.length -1;
+        }
+        // 发送广播停止前台Activity更新界面
+        Intent intent = new Intent();
+        intent.putExtra("current", index);
+        intent.setAction(MUSICBOX_ACTION);
+        sendBroadcast(intent);
+        try {
+            // 获取assets目录下指定文件的AssetFileDescriptor对象
+            AssetFileDescriptor assetFileDescriptor = assetManager.openFd(musics[current]);
+            mediaPlayer.reset(); // 初始化mediaPlayer对象
+            mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(),
+                    assetFileDescriptor.getStartOffset(),
+                    assetFileDescriptor.getLength());
+            // 准备播放音乐
+            mediaPlayer.prepare();
+            // 播放音乐
+            mediaPlayer.start();
+            // getDuration() 方法要在prepare() 方法之后, 否则会出现异常
+            MainActivity.skbMusic.setMax(mediaPlayer.getDuration()); // 设置SeekBar的长度
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        // -------- 定时器记录播放进度------------
+        Timer timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                isTimerRunning = true;
+                if (isChanging == true) // 当用户正在拖动进度进条时不处理进度条的进度
+                    return;
+                MainActivity.skbMusic.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        };
+        // 每隔10毫秒检测一下播放进度
+        timer.schedule(timerTask, 0, 10);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    class MusicServiceReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int control = intent.getIntExtra("control", -1);
+            switch (control) {
+                case STATE_PLAY: // 播放音乐
+                    if (state == STATE_PAUSE) { // 如果原来状态是暂停
+                        mediaPlayer.start();
+                    } else if (state != STATE_PLAY) {
+                        prepareAndPlay(current);
+                    }
+                    state = STATE_PLAY;
+                    break;
+                case STATE_PAUSE: // 暂停播放
+                    if (state == STATE_PLAY) {
+                        mediaPlayer.pause();
+                        state = STATE_PAUSE;
+                    }
+                    break;
+                case STATE_STOP: // 停止播放
+                    if (state == STATE_PLAY || state == STATE_PAUSE) {
+                        state = STATE_STOP;
+                        mediaPlayer.seekTo(0);
+                        mediaPlayer.stop();
+                    }
+                    break;
+                case STATE_PREVIOUS: // 上一首
+                    prepareAndPlay(--current);
+                    state = STATE_PLAY;
+                    break;
+                case STATE_NEXT: // 下一首
+                    prepareAndPlay(++current);
+                    state = STATE_PLAY;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(receiver);
+        timerTask.cancel();
+        mediaPlayer.release();
+        super.onDestroy();
+    }
+}
